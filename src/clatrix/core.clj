9b4c625cd49c57cf830c393ee36b86df51385ff4
Joseph Abrahamson <abrahamson.j@gmail.com>
2012-04-27 19:07:33 -0400

    * src/clatrix/core.clj: More small changes to documentation.

--- src/clatrix/core.clj
+++ src/clatrix/core.clj
@@ -426,7 +426,7 @@
 ;;;
 ;;;     (slice A _ 1 (column (range 10)))
 ;;;
-;;; replaces the 2nd column of `A` with `[0 1 2 3 4 5 6 7 8 9]`.
+;;; replaces the 2nd column of `A` with the numbers 0 through 9.
 
 (defn- slicer
   ([^Matrix matrix rowspec colspec]
