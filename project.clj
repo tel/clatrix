a3d7ea460d987b31584f6d2bfe38fe9575991c39
Joseph Abrahamson <abrahamson.j@gmail.com>
2012-04-27 19:01:21 -0400

    * project.clj: It wasn't really a requirement to have vimclojure in dev-dependencies.

--- project.clj
+++ project.clj
@@ -7,5 +7,4 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [slingshot "0.8.0"]
                  [jblas/jblas "1.2.1"]
-                 [jblas/native "1.2.0"]]
-  :profiles {:dev {:dependencies [[vimclojure/server "2.3.1"]]}})
+                 [jblas/native "1.2.0"]])
