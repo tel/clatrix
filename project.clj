d6b791e8569af40525b26a9420e870459a34ea59
Paul Lam <paul@quantisan.com>
2012-11-25 10:19:41 +0100

    change groupID

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clatrix/clatrix "0.1.0"
+(defproject com.quantisan/clatrix "0.1.1-SNAPSHOT"
   :description "Because using matrices in Clojure needs to not suck."
   :url "https://github.com/tel/clatrix"
   :license {:name "MIT License"
