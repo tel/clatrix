629579e6ad3b34152f578353d4793676043406dc
Paul Lam <paul@quantisan.com>
2012-12-04 19:42:00 +0000

    changed enough for 0.2.0-SNAPSHOT instead

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.quantisan/clatrix "0.1.1-SNAPSHOT"
+(defproject com.quantisan/clatrix "0.2.0-SNAPSHOT"
   :description "Because using matrices in Clojure needs to not suck."
   :url "https://github.com/tel/clatrix"
   :license {:name "MIT License"
