df4f5c78b54a1fcd75c36c58c6e2af8cea7c9fc7
Paul Lam <paul@quantisan.com>
2012-12-17 18:17:51 +0000

    use org.jblas/jblas 1.2.2

--- project.clj
+++ project.clj
@@ -9,6 +9,5 @@
                      [expectations "1.4.16"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [slingshot "0.8.0"]
-                 [com.quantisan/jblas "1.2.2-SNAPSHOT"]
-                 [jblas/native "1.2.0"]]
+                 [org.jblas/jblas "1.2.2"]]
   :profiles {:dev {:dependencies [[expectations "1.4.16"]]}})
