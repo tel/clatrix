440c43e511d83fb499166fccaeda683fcb8dd76d
Paul Lam <paul@quantisan.com>
2012-12-24 21:03:24 +0000

    release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.quantisan/clatrix "0.2.2-SNAPSHOT"
+(defproject com.quantisan/clatrix "0.2.2"
   :description "Because using matrices in Clojure needs to not suck."
   :url "http://quantisan.github.com/clatrix"
   :license {:name "MIT License"
