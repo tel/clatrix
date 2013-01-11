922f0be45f9b5683b0a4390efdc113d8afb67d38
Paul Lam <paul@quantisan.com>
2013-01-11 21:57:09 +0000

    add dep on matrix-api

--- project.clj
+++ project.clj
@@ -9,6 +9,6 @@
                      [expectations "1.4.16"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [slingshot "0.8.0"]
-                 [org.jblas/jblas "1.2.2"]]
-  :profiles {:dev {:dependencies [[expectations "1.4.16"]]}}
-)
+                 [org.jblas/jblas "1.2.2"]
+                 [mikera/matrix-api "0.0.2-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[expectations "1.4.16"]]}})
