d0ca2ea063de86fa03693c4f59383a72be976b34
Paul Lam <paul@quantisan.com>
2013-01-13 13:50:19 +0000

    use matrix-api 0.0.2 release

--- project.clj
+++ project.clj
@@ -10,5 +10,5 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [slingshot "0.8.0"]
                  [org.jblas/jblas "1.2.2"]
-                 [mikera/matrix-api "0.0.2-SNAPSHOT"]]
+                 [mikera/matrix-api "0.0.2"]]
   :profiles {:dev {:dependencies [[expectations "1.4.16"]]}})
