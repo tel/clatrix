4e29380cac4df02e6bf4906d4151e882ab19e81e
mikera <mike.r.anderson.13@gmail.com>
2013-05-27 14:02:43 +0800

    update project.clj with dependencies

--- project.clj
+++ project.clj
@@ -11,4 +11,5 @@
                  [slingshot "0.10.3"]
                  [org.jblas/jblas "1.2.3"]
                  [net.mikera/core.matrix "0.7.1"]]
-  :profiles {:dev {:dependencies [[expectations "1.4.16"]]}})
+  :profiles {:dev {:dependencies [[criterium/criterium "0.4.1"]
+                                  [expectations "1.4.41"]]}})
