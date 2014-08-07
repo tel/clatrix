53926db32a2bbd4e1f76ba0345ea55b5af5d7f81
mikera <mike.r.anderson.13@gmail.com>
2014-08-07 16:52:25 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
   :resource-paths ["native"]
   :plugins [[lein-expectations "0.0.8"]]
   :dev-dependencies [[lein-expectations "0.0.8"]
-                     [expectations "1.4.41"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]
+                     [expectations "2.0.9"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [slingshot "0.10.3"]
                  [org.jblas/jblas "1.2.3"]
                  [net.mikera/core.matrix "0.27.1"]]
   
-  :profiles {:dev {:dependencies [[criterium/criterium "0.4.1"]
-                                  [expectations "1.4.41"]]}})
+  :profiles {:dev {:dependencies [[criterium/criterium "0.4.3"]
+                                  [expectations "1.4.56"]]}})
