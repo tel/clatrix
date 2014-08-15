61760b11af945f36f6ba14c99d3b94b24a8dec16
mikera <mike.r.anderson.13@gmail.com>
2014-08-15 19:29:53 +0800

    update core.matrix version

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [slingshot "0.10.3"]
                  [org.jblas/jblas "1.2.3"]
-                 [net.mikera/core.matrix "0.28.0"]]
+                 [net.mikera/core.matrix "0.29.1"]]
   
   :profiles {:dev {:dependencies [[criterium/criterium "0.4.3"]
                                   [expectations "1.4.56"]]}})
