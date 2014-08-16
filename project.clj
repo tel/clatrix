146aa303fbcb5b358a44817f34b7e183d2e064a1
mikera <mike.r.anderson.13@gmail.com>
2014-08-16 19:28:37 +0800

    create next snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clatrix/clatrix "0.4.0"
+(defproject clatrix/clatrix "0.4.1-SNAPSHOT"
   :description "Because using matrices in Clojure needs to not suck."
   :url "http://tel.github.com/clatrix"
   :license {:name "MIT License"
